49e6c0e09427ce452af5ef59a9851ed5ca6d48a0
denyncrawford <kiquerangelb@gmail.com>
2020-12-20 22:56:27 -0300

    adding styles to build final

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
+import './assets/themes/dndb/index.css';
 import locale from 'element-plus/lib/locale/lang/es'
 import App from './App.vue'
 import './assets/styles/tailwind.index.css'
+import 'animate.css/animate.min.css'
 import VueHighlightJS from 'vue3-highlightjs'
 
 const app = createApp(App)
