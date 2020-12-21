d650f197d22b26280313b292bc189cf2fe9efa6b
denyncrawford <kiquerangelb@gmail.com>
2020-12-20 22:00:25 -0300

    adding styles to build

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ import 'element-plus/lib/theme-chalk/index.css';
 import './assets/themes/dndb/index.css';
 import locale from 'element-plus/lib/locale/lang/es'
 import App from './App.vue'
+import './assets/styles/tailwind.index.css'
 import './assets/styles/index.css'
 import 'animate.css/animate.min.css'
 import VueHighlightJS from 'vue3-highlightjs'
 import 'highlight.js/styles/dracula.css'
-import './index.css'
 
 const app = createApp(App)
 app.use(router)
