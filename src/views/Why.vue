601bbabb55fea751fea42fbabb7828ef1b35c1c7
Arnau <agomez1f@gmail.com>
2020-12-22 22:19:00 +0100

    Added Knuckles icon 🖼

--- src/views/Why.vue
+++ src/views/Why.vue
@@ -6,7 +6,7 @@
         <p class="mt-10 max-w-prose">This project arises from the need for a database for Deno. Now there are a few :D, but at the time we were pioneers. 
         <br><br>
         We always keep advancing and improving DnDB, making it a reliable and stable module for production.</p>
-        <h1 class="text-main-color mt-32 text-5xl max-w-xl">Is DnDB da wae?</h1>
+        <h1 class="text-main-color mt-32 text-5xl max-w-xl">Is DnDB da wae? <img class="inline h-12" src="../assets/da-wae-kuckles-icon.png" alt="Knuckles icon"></h1>
         <p class="mt-10 max-w-prose">DnDB is an optimized data store made for the Deno runtime, with no bottlenecks or exaggerated load times. This is made possible by not holding all the data in memory.
         <br><br>
         Right now all the 'databases' and data stores available for Deno are loading (into memory) the entire collection to read and write the data. This is not an optimal solution when it comes to a production environment where data tends to grow. 
@@ -19,7 +19,7 @@
         <br><br>
         If you need something ready for production, there are some well known driver APIS available at Deno third parties too.
         But if you want to support the Deno comunity by creating awesome stuff with our awesome stuff...
-        <span class="text-main-color">This is the wae</span>.
+        <span class="text-main-color">This is da wae</span>.
         </p>
       </div>
     </page>
